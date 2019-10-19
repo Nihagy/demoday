fab98196fc1ad79df5b8e4fe107131a9cf603f9e
Nihagy <valdenor.michael@gmail.com>
2019-10-19 19:30:49 -0300

    linkar e organizar

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todas_por_todas import views
 
+
+# troca o caminho das url pelo caminha que ta com aspas
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index),
+    path('login/', views.login),
 ]
--- todas_por_todas/static/js/teste1.js
+++ todas_por_todas/static/js/teste1.js
@@ -1,6 +1,6 @@
 let cabecalho = document.querySelector(`header`)
-let logo = document.querySelector(`.logo-max img`)
-let navLetra = document.querySelectorAll(`.nav-max`)[0]
+let logo = document.querySelector(`.logo_max img`)
+let navLetra = document.querySelectorAll(`.nav_max`)[0]
 let menu = document.querySelectorAll(`.menu`)[0]
 let traco1 = document.querySelectorAll(`span`)[0]
 let traco2 = document.querySelectorAll(`span`)[1]
@@ -8,33 +8,30 @@ let traco3 = document.querySelectorAll(`span`)[2]
 
 
 window.addEventListener(`scroll`,rolagem, false)
-
 function rolagem(){
     
-    if(window.pageYOffset > 60 && cabecalho.classList.contains(`header-max`)){
-        //efeito na nav bar
-        cabecalho.classList.remove(`header-max`)
-        cabecalho.classList.add(`header-min`)
+    if(window.pageYOffset > 60 && cabecalho.classList.contains(`tam_max`)){
+        cabecalho.classList.remove(`tam_max`)
+        cabecalho.classList.add(`tam_min`)
         // logo.setAttribute(`src`, `../images/login.png `)
-        logo.classList.remove(`logo-max`)
-        logo.classList.add(`logo-min`)
-        navLetra.classList.remove(`nav-max`)
-        navLetra.classList.add(`nav-min`)
-        //efeito a listras do menu
+        logo.classList.remove(`logo_max`)
+        logo.classList.add(`logo_min`)
+        navLetra.classList.remove(`nav_max`)
+        navLetra.classList.add(`nav_min`)
         menu.firstElementChild.classList.remove(`menu-max`)
         menu.firstElementChild.classList.add(`menu-min`)
         traco2.classList.remove(`menu-max`)
         traco2.classList.add(`menu-min`)
         traco3.classList.remove(`menu-max`)
         traco3.classList.add(`menu-min`)
-    }else if(pageYOffset <= 60 && cabecalho.classList.contains(`header-min`)) {
-        cabecalho.classList.remove(`header-min`)
-        cabecalho.classList.add(`header-max`)
+    }else if(pageYOffset <= 60 && cabecalho.classList.contains(`tam_min`)) {
+        cabecalho.classList.remove(`tam_min`)
+        cabecalho.classList.add(`tam_max`)
         // logo.setAttribute(`src`, `{% static 'images/teste.png' %}`)
-        logo.classList.remove(`logo-min`)
-        logo.classList.add(`logo-max`)
-        navLetra.classList.remove(`nav-min`)
-        navLetra.classList.add(`nav-max`)
+        logo.classList.remove(`logo_min`)
+        logo.classList.add(`logo_max`)
+        navLetra.classList.remove(`nav_min`)
+        navLetra.classList.add(`nav_max`)
         menu.firstElementChild.classList.remove(`menu-min`)
         menu.firstElementChild.classList.add(`menu-max`)
         traco2.classList.add(`menu-max`)
@@ -43,16 +40,28 @@ function rolagem(){
         traco3.classList.remove(`menu-min`)
     }
 
+
+}
+
+
+
+window.addEventListener(`scroll`,rolarSimb, false)
+function rolarSimb(){
+    if(cabecalho.classList.contains(`tam_min`) && navLetra.classList.contains("menu-visivel")){
+        circulo.style.color= "#2980b9";
+    }else if(cabecalho.classList.contains(`tam_max`) && navLetra.classList.contains("menu-visivel")){
+        circulo.style.color = `#c5eff7`
+    }
 }
 
 
 menu.addEventListener(`click`,mostrarMenu,false)
-// abre e fecha o menu
+
+
 function mostrarMenu(){
     navLetra.classList.toggle("menu-visivel")
 }
 
-// faz o menu e para seção que foi clicado e fecha o menu
 navLetra.addEventListener(`click`, navegarMenu, false)
 function navegarMenu(event){
     if (event.target.tagName == 'A'){
